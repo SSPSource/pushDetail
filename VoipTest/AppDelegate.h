0d26038e4366603e4ca270d0a2086f653571922b
Sunsp <diudiu2015@users.noreply.github.com>
2017-09-29 16:59:11 +0800

    功能--pushKit

--- VoipTest/AppDelegate.h
+++ VoipTest/AppDelegate.h
@@ -7,8 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <PushKit/PushKit.h>
+#ifdef NSFoundationVersionNumber_iOS_9_x_Max
+#import <UserNotifications/UserNotifications.h>
+#endif
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate,PKPushRegistryDelegate,UNUserNotificationCenterDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
--- VoipTest/AppDelegate.m
+++ VoipTest/AppDelegate.m
@@ -8,6 +8,7 @@
 
 #import "AppDelegate.h"
 
+
 @interface AppDelegate ()
 
 @end
@@ -16,8 +17,159 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [self registerPushDemo:application];
+   [self registerPushKit];
     return YES;
+}
+#pragma mark - 普通推送
+-(void)registerPushDemo:(UIApplication *)application{
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 10.0) {
+        //iOS10特有
+        if (@available(iOS 10.0, *)) {
+            UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+            // 必须写代理，不然无法监听通知的接收与点击
+            center.delegate = self;
+            [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert | UNAuthorizationOptionBadge | UNAuthorizationOptionSound) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+                if (granted) {
+                    // 点击允许
+                    NSLog(@"注册成功");
+                    
+                    [center getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+                        NSLog(@"%@", settings);
+                        
+                    }];
+                } else {
+                    // 点击不允许
+                    NSLog(@"注册失败");
+                }
+            }];
+        } else {
+            // Fallback on earlier versions
+        }
+    }else if ([[UIDevice currentDevice].systemVersion floatValue] >8.0){
+        //iOS8 - iOS10
+        [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeAlert | UIUserNotificationTypeSound | UIUserNotificationTypeBadge categories:nil]];
+        
+    }else if ([[UIDevice currentDevice].systemVersion floatValue] < 8.0) {
+        //iOS8系统以下
+        //        [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound];
+    }
+    // 注册获得device Token
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+}
+- (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings{
+    [application registerForRemoteNotifications];//必须先实现这个方法，才会走下面的方法
+}
+
+//静默push必须实现下面协议
+//而当程序处于后台或者被杀死状态，收到远程通知后，当你进入(aunch)程序时
+//实现此方法didReceiveRemoteNotification将不会被调用
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandler{
+    NSLog(@"====%s===userInfo==%@",__func__,userInfo);
+    NSLog(@"iOS7及以上系统，收到通知:%@", userInfo);
+    completionHandler(UIBackgroundFetchResultNewData);
+    
+}
+
+
+
+//应用在运行状态（也就是打卡状态），收到远程推送
+//iOS10以下使用这个方法接收通知
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    NSLog(@"====%s===",__func__);
+   
+    NSLog(@"iOS6及以下系统，收到通知:%@",userInfo );
+    //        self.userInfo = userInfo;
+    //定制自定的的弹出框
+    if([UIApplication sharedApplication].applicationState == UIApplicationStateActive)
+    {
+//        UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"标题"
+//                                                            message:@"Test On ApplicationStateActive"
+//                                                           delegate:self
+//                                                  cancelButtonTitle:@"确定"
+//                                                  otherButtonTitles:nil];
+//
+//        [alertView show];
+        
+    }
+}
+#ifdef NSFoundationVersionNumber_iOS_10_x_Max
+//iOS10新增：处理前台收到通知的代理方法
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler{
+    NSDictionary * userInfo = notification.request.content.userInfo;
+    if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+        NSLog(@"====%s===userInfo=%@",__func__,userInfo);
+        //应用处于前台时的远程推送接受
+        
+        
+    }else{
+        //应用处于前台时的本地推送接受
+    }
+    //当应用处于前台时提示设置，需要哪个可以设置哪一个
+    //    completionHandler(UNNotificationPresentationOptionSound|UNNotificationPresentationOptionBadge|UNNotificationPresentationOptionAlert);
+    
+    
+}
+
+//iOS10新增：处理后台点击通知的代理方法
+-(void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler{
+    
+    NSLog(@"====%s===",__func__);
+    NSDictionary * userInfo = response.notification.request.content.userInfo;
+    if (@available(iOS 10.0, *)) {
+        if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+            //应用处于后台时的远程推送接受
+            //必须加这句代码
+           
+        }else{
+            //应用处于后台时的本地推送接受
+        }
+    } else {
+        // Fallback on earlier versions
+    }
+}
+#endif
+// 获得Device Token
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken{
+    
+    NSString *token = [NSString stringWithFormat:@"%@", [[[[deviceToken description] stringByReplacingOccurrencesOfString: @"<" withString: @""]                  stringByReplacingOccurrencesOfString: @">" withString: @""]                 stringByReplacingOccurrencesOfString: @" " withString: @""]];
+    //获取终端设备标识，这个标识需要通过接口发送到服务器端，服务器端推送消息到APNS时需要知道终端的标识，APNS通过注册的终端标识找到终端设备
+    NSLog(@"==token=====%@",token);
+}
+// 获得Device Token失败
+- (void)application:(UIApplication *)application
+didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"did Fail To Register For Remote Notifications With Error: %@", error);
+}
+#pragma mark - pushkit
+
+-(void)registerPushKit{
+   NSLog(@"====registerPushKit===");
+    PKPushRegistry *pushRegistry = [[PKPushRegistry alloc] initWithQueue:nil];
+    pushRegistry.delegate = self;
+    pushRegistry.desiredPushTypes = [NSSet setWithObject:PKPushTypeVoIP];
+ 
+}
+
+
+
+
+- (void)pushRegistry:(PKPushRegistry *)registry didUpdatePushCredentials:(PKPushCredentials *)credentials forType:(NSString *)type{
+    NSString *str = [NSString stringWithFormat:@"%@",credentials.token];
+    NSString* tokenStr = [[[str stringByReplacingOccurrencesOfString:@"<" withString:@""]
+                           stringByReplacingOccurrencesOfString:@">" withString:@""] stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSLog(@"==pushkit=token==%@",tokenStr);
+} //这个代理方法是获取了设备的唯tokenStr，是要给服务器的
+- (void)pushRegistry:(PKPushRegistry *)registry didInvalidatePushTokenForType:(PKPushType)type{
+    NSLog(@"===%s===",__func__);
+    
+}
+- (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(PKPushType)type withCompletionHandler:(void(^)(void))completion NS_AVAILABLE_IOS(11_0){
+    NSLog(@"===%s===",__func__);
+}
+- (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(PKPushType)type NS_DEPRECATED_IOS(8_0, 11_0){
+    NSLog(@"===%s===",__func__);
 }
 
 
